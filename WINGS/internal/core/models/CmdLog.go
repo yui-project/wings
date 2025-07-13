fc765c7e548b9c7b193f0c6d8393f5c8166e570d
whatacotton <whatacotton@gmail.com>
2025-07-13 21:06:13 +0900

    change struct type

--- WINGS/internal/core/models/CmdLog.go
+++ WINGS/internal/core/models/CmdLog.go
@@ -9,12 +9,12 @@ type CmdLog struct {
 	ExecTimeDouble float64
 	ExecTimeStr    string
 	CmdName        string
-	Param1         string
-	Param2         string
-	Param3         string
-	Param4         string
-	Param5         string
-	Param6         string
+	Param1         *string
+	Param2         *string
+	Param3         *string
+	Param4         *string
+	Param5         *string
+	Param6         *string
 	OperationId    string
 	Operation      Operation
 }
